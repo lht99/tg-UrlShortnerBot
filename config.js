45899545912963686d57a5322b94356027391de0
lht99 <33472127+lht99@users.noreply.github.com>
2020-11-12 08:37:20 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    botToken:'1413295215:AAFqhQABub-oy9aPH_y_VaRtWMJ308KDG4Q ',
+    botToken:'1413295215:AAFqhQABub-oy9aPH_y_VaRtWMJ308KDG4Q',
     bitlyToken:'dfccaf2a5914c746fd3e61f4253e950c35613ce1'
 };
