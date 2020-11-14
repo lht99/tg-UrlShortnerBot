eae28987d5e8e3fbd22de3d4532aa9e6125c3c5b
lht99 <33472127+lht99@users.noreply.github.com>
2020-11-15 01:23:38 +0900

    Create anh.js

---
+++ commands/anh.js
@@ -0,0 +1,12 @@
+const TeleBot = require('telebot');
+const config = require('./config');
+const bot = new TeleBot(config.botToken);
+const chat_id = user_id("Me")
+const photo_url = "https://telegram.org/img/t_logo.png"
+
+bot.sendPhoto(
+  chat_id = chat_id,
+  photo = photo_url,
+  caption = "Telegram Logo"
+);
+module.exports = anh;
