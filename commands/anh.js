6587948f6d8b6a038e8b071616d558549254c7d4
lht99 <33472127+lht99@users.noreply.github.com>
2020-11-15 11:02:03 +0900

    Update anh.js

--- commands/anh.js
+++ commands/anh.js
@@ -4,7 +4,7 @@ const bot = new TeleBot(config.botToken);
 const photo_url = "https://telegram.org/img/t_logo.png"
 
 bot.sendPhoto(
-  chatId: msg.message.chat_id,
+  chatId: msg.message.chat.id,
   photo = photo_url,
   caption = "Telegram Logo"
 );
