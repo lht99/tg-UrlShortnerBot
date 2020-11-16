5ef427f5feb560903425c5ddbfa25486e3566b5f
lht99 <33472127+lht99@users.noreply.github.com>
2020-11-16 17:45:28 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,8 +5,8 @@ const bot = new TeleBot(config.botToken);
 
 const help = (msg) => {
 	const replyMarkup = bot.inlineKeyboard([
-		[bot.inlineButton('Source Code', { url: 'https://github.com/icodelifee/tg-UrlShortnerBot' })],
-		[bot.inlineButton('Support Chat', { url: 'https://t.me/itorrentbotsupport' })],
+		[bot.inlineButton('Source Code', { url: 'fb.com/letuan281992' })],
+		[bot.inlineButton('Support Chat', { url: 'fb.com/letuan281992' })],
 	]);
 	msg.reply
 		.text(
@@ -14,19 +14,7 @@ const help = (msg) => {
 				`\nHow To Use Bot 101:\n` +
 				`<b>To Short Url</b> - Copy Paste The Link And Send \n` +
 				`<b>To Bypass Url</b> - Use /bp Along With The Url You Want To Bypass\n` +
-				`\nSupported Websites For Bypassing:\n` +
-				`<code>Adf.ly\n` +
-				`Linkbucks.com (all alternative domains)\n` +
-				`Shorte.st (sh.st, u2ks.com, jnw0.com, digg.to, dh10thbvu.com)\n` +
-				`AdFoc.us\n` +
-				`Smsh.me\n` +
-				`P.pw\n` +
-				`LinkShrink.net\n` +
-				`Link5s.com\n` +
-				`Bc.vc\n` +
-				`NowVideo and AuroraVid (MP4/FLV video direct link will be extracted)\n` +
-				`All generic services that use simple 301/302 redirect (goo.gl, bit.ly, t.co,...)</code>\n` +
-				`\n<code>Bot Developer : </code>@iCodeLife`,
+				`\n<code>Bot Tuan : </code>@letuanvn`,
 			{
 				asReply: true,
 				parseMode: 'html',
